c9c76de9d8f38d13cb46a69080f072b183d097be
Hoang Tan <htan1901@gmail.com>
2022-05-28 20:49:04 +0700

    dieu chinh layout them_toa_thuoc_activity

--- app/src/main/java/com/example/myapplication/activities/ThemToaThuocActivity.java
+++ app/src/main/java/com/example/myapplication/activities/ThemToaThuocActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication.activities;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,5 +14,9 @@ public class ThemToaThuocActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_them_toa_thuoc);
+        ImageButton doneButton = findViewById(R.id.them_toa_thuoc_activity_xong);
+        ImageButton themThuocButton = findViewById(R.id.them_toa_thuoc_activity_them_thuoc_button);
+        ImageButton navigateNhapButton = findViewById(R.id.them_toa_thuoc_activity_navigate_nhap);
+        ImageButton navigateChupButton = findViewById(R.id.them_toa_thuoc_activity_navigate_chup);
     }
 }
